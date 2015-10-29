d16205fefea6fa1b9b8742916c1b0846d4b91663
Luis Daniel <ldcamposh@gmail.com>
2015-10-29 17:51:46 -0600

    Trabajos DevC++

---
+++ Desktop/FUP1C/multiplicacion_sumando.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int i;
+int f;
+int suma;
+
+int main()
+
+{
+	printf("Introducir primer numero\n");
+	scanf("%d",&i);
+	
+	printf("Introducir segundo numero\n");
+	scanf("%d",&f);
+	
+
+	while (i)
+	
+	{
+	suma=
+	}
+}
